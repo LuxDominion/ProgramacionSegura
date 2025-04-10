d474bf6e89f37d95c849244748ac2d6ac6eae312
LuxDominion <Cesaraedo987@gmail.com>
2025-04-10 19:07:35 -0400

    New

---
+++ Funciones/Funciones.py
@@ -0,0 +1,5 @@
+def saludar(nombre):
+    print(f"Buenos Dias Estimado(a) {nombre}")
+    
+nombre = input("Ingrese su nombre:")
+saludar(nombre)
--- bucles/bucle_for.py
+++ bucles/bucle_for.py
@@ -2,6 +2,12 @@
 print()
 Juegos = ["Dota 2","MK","Street Figther","Counter Strike"]
 numeros = [10,20,30,40,50]
+diccionario = {
+   "nombre":"Lux",
+   "apellido":"Dominion",
+    "edad":"19",
+    "estudiante":False
+}
 
 for juego in Juegos:
     print(juego)
@@ -27,3 +33,18 @@ for num in range(5,15):
         indice = elemento[0]
         valor = elemento[1]
         print(f"El indice es: {indice} y el valor es: {valor}")
+
+    for elemento in diccionario:
+        print(f"La Clave del dato es: {elemento}")
+
+    for elemento in diccionario.items():
+        clave = elemento [0]
+        valor = elemento [1]
+        print(f"La Clave del dato es: '{clave}' y el valor es' {valor}'")
+
+conjunto = {"Dominio de luz",30,True,"Comandos",23}
+
+print()
+for elemento in conjunto:
+     if type(elemento) == str:
+      print(elemento)
