98926733a88d94af28541457536cb78c700ab4ca
LuxDominion <Cesaraedo987@gmail.com>
2025-05-06 18:23:42 -0400

    new

---
+++ modulos/cuadrilatero.py
@@ -0,0 +1,14 @@
+# Definir 3 funciones.... calculo de perimetro, area y volumen
+
+def perimetro(ancho,largo):
+    return ancho + largo * 2
+
+def area(ancho,largo):
+    return ancho * largo
+
+def volumen(ancho,largo,alto):
+    return ancho * largo * alto 
+
+print(perimetro(2,3))
+print(area(2,3))
+print(volumen(2,3,4))
---
+++ modulos/principal.py
@@ -0,0 +1,5 @@
+import cuadrilatero
+
+print(cuadrilatero.perimetro(2,3))
+print(cuadrilatero.area(2,3))
+print(cuadrilatero.volumen(2,3,4))
