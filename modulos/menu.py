a3ff4375eabd3d2b74330cb81874768932740c74
LuxDominion <Cesaraedo987@gmail.com>
2025-05-13 17:56:20 -0400

    new a

--- modulos/menu.py
+++ modulos/menu.py
@@ -1,3 +1,4 @@
+
 import cuadrilatero
 import circuferencia
 
@@ -13,32 +14,40 @@ def menu_principal():
 def sub_menu():
     print()
     print("¿Para que figura Geometrica?")
-    print("1: Cuadrilatero" )
+    print("1: Cuadrilatero")
     print("2: Circuferencia")
     print("0: Salir")
     print()
 
 def programa_principal():
     while True:
-    opcion = input("Seleccione su opcion (0-3):")
+        menu_principal()
+        opcion = input("Seleccione su opcion (0-3): ")
 
-    if opcion == "1":
-        sub_menu()opcion_sub_menu = input("Seleccione su Opcion (0-3): ")
-        if opcion_sub_menu == "1"
-        ancho = input("Ingrese el Ancho")
-        largo = input("Ingrese su Largo")
-        print(funciones.cuadrilatero.perimetro(radio))
-    elif opcion_sub_menu == "0":
-        return
-    else:
-        print("Opcion Invalida!")
-    elif opcion == "2":
-        pass
-    elif opcion == "3": 
-        pass
-    elif opcion == "0":
-        print("saliendo del sistema...")
-        break
-    else:
-        print("Opcion Fallida...")
+        if opcion == "1":
+            sub_menu()
+            opcion_sub_menu = input("Seleccione su Opcion (0-2): ")
+            if opcion_sub_menu == "1":
+                ancho = float(input("Ingrese el Ancho: "))
+                largo = float(input("Ingrese su Largo: "))
+                print(cuadrilatero.perimetro(ancho, largo))
+            elif opcion_sub_menu == "2":
+                radio = float(input("Ingrese el radio: "))
+                print(circuferencia.perimetro(radio))
+            elif opcion_sub_menu == "0":
+                continue
+            else:
+                print("Opcion Invalida!")
+        elif opcion == "2":
+            # Add area calculation logic here
+            pass
+        elif opcion == "3": 
+            # Add volume calculation logic here
+            pass
+        elif opcion == "0":
+            print("saliendo del sistema...")
+            break
+        else:
+            print("Opcion Fallida...")
 
+programa_principal()
