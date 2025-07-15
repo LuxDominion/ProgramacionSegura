3fde6a286f724e853de93ea5fa37e43efeb72ce7
LuxDominion <Cesaraedo987@gmail.com>
2025-07-15 17:06:39 -0400

    kjki

--- manejo_notas/data/conexion.py
+++ manejo_notas/data/conexion.py
@@ -16,6 +16,14 @@ def ejecutar_consulta(consulta):
     resultado = cursor.fetchall()
     return resultado
 
+def insertar_datos(consulta, datos):
+    cursor.execute(consulta, datos)
+    id = cursor.lastrowid
+    conexion.commit()
+    cursor.close()
+    conexion.close()
+    
+
 # cursor.execute('SELECT numero_opcion,opcion_menu FROM opciones_menu')
 # resultado = cursor.fetchall()
 # print(resultado)
